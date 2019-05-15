247254f7fdfea6bf630b94f2e6ee323589fc3a6b
Paul <paul@zom.bi>
2019-05-15 19:08:24 +0200

    Update SCS session manager

--- services/sessions.go
+++ services/sessions.go
@@ -2,6 +2,7 @@ package services
 
 import (
 	"encoding/gob"
+	"errors"
 	"fmt"
 	"html/template"
 	"log"
@@ -11,7 +12,7 @@ import (
 	"github.com/alexedwards/scs"
 )
 
-var (
+const (
 	// FlashesKey is the key used for the flashes in the cookie
 	FlashesKey = "_flashes"
 	// UserEmailKey is the key used to reference usernames
@@ -24,29 +25,24 @@ func init() {
 }
 
 type SessionsConfig struct {
-	SessionName string
-	CookieKey   string
-	HttpOnly    bool
-	Secure      bool
-	Lifetime    time.Duration
+	HTTPOnly bool
+	Secure   bool
+	Lifetime time.Duration
 }
 
 // Sessions is a wrapped scs.Store in order to implement custom logic
 type Sessions struct {
-	*scs.Manager
+	*scs.Session
 }
 
 // NewSessions populates the default sessions Store
 func NewSessions(conf *SessionsConfig) *Sessions {
-	store := scs.NewCookieManager(
-		conf.CookieKey,
-	)
-	store.Name(conf.SessionName)
-	store.HttpOnly(true)
-	store.Lifetime(conf.Lifetime)
-	store.Secure(conf.Secure)
+	session := scs.NewSession()
+	session.Lifetime = conf.Lifetime
+	session.Cookie.HttpOnly = true
+	session.Cookie.Secure = conf.Secure
 
-	return &Sessions{store}
+	return &Sessions{session}
 }
 
 func (store *Sessions) GetUsername(req *http.Request) string {
@@ -56,17 +52,8 @@ func (store *Sessions) GetUsername(req *http.Request) string {
 		return ""
 	}
 
-	sess := store.Load(req)
-
-	email, err := sess.GetString(UserEmailKey)
-	if err != nil {
-		// Username found
-		return ""
-
-	}
-
-	// User is logged in
-	return email
+	email := store.GetString(req.Context(), UserEmailKey)
+	return email // "" if no user is logged in
 }
 
 func (store *Sessions) SetUsername(w http.ResponseWriter, req *http.Request, username string) {
@@ -75,13 +62,10 @@ func (store *Sessions) SetUsername(w http.ResponseWriter, req *http.Request, use
 		return
 	}
 
-	sess := store.Load(req)
-
 	// renew token to avoid session pinning/fixation attack
-	sess.RenewToken(w)
+	store.RenewToken(req.Context())
 
-	sess.PutString(w, UserEmailKey, username)
-
+	store.Put(req.Context(), UserEmailKey, username)
 }
 
 type Flash struct {
@@ -101,23 +85,20 @@ func (flash Flash) Render() template.HTML {
 
 // Flash add flash message to session data
 func (store *Sessions) Flash(w http.ResponseWriter, req *http.Request, flash Flash) error {
-	var flashes []Flash
-
-	sess := store.Load(req)
-
-	if err := sess.GetObject(FlashesKey, &flashes); err != nil {
-		return err
+	flashes, ok := store.Get(req.Context(), FlashesKey).([]Flash)
+	if !ok {
+		return errors.New("Could not get flashes")
 	}
 
 	flashes = append(flashes, flash)
-
-	return sess.PutObject(w, FlashesKey, flashes)
+	return nil
 }
 
 // Flashes returns a slice of flash messages from session data
 func (store *Sessions) Flashes(w http.ResponseWriter, req *http.Request) []Flash {
-	var flashes []Flash
-	sess := store.Load(req)
-	sess.PopObject(w, FlashesKey, &flashes)
+	flashes, ok := store.Pop(req.Context(), FlashesKey).([]Flash)
+	if !ok {
+		return nil
+	}
 	return flashes
 }
